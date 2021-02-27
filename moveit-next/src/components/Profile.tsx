9117f9c3817aac94d2452213cad8bd9071305fb3
Vince <l.vicente9003@gmail.com>
2021-02-27 13:13:10 -0300

    finished day-2

--- moveit-next/src/components/Profile.tsx
+++ moveit-next/src/components/Profile.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 
+import styles from '../styles/components/Profile.module.css'
+
+
 function Profile() {
     return (
-        <div>
+        <div className={styles.profileContainer}>
             <img src="https://github.com/LucasHVicente.png" alt="Vince"/>
             <div>
                 <strong>Lucas Vicente</strong>
+                <p>
+                    <img src="icons/level.svg" alt="level"/>
+                    Level 1</p>
             </div>
         </div>
     )
